04f284e3729460a94ed77c5da44f35f538e48b11
Manish P <invincible.manish1417@gmail.com>
2022-02-20 14:20:06 +0530

    prettified the code

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
+from datetime import datetime
+from dotenv import load_dotenv
+import bson
+from bson.objectid import ObjectId
 import flask
 import os
 import json
-from datetime import datetime
-from dotenv import load_dotenv
 import pymongo
-from bson.objectid import ObjectId
-#from models.bank import BankTransactionDetail
-#from schemas.bank import serializeDict, serializeList
 
 load_dotenv()
-
 
 MONGO_KEY = os.getenv('MONGO_URI')
 client = pymongo.MongoClient(MONGO_KEY)
 db = client["bankCallify"]
+
 app = flask.Flask(__name__)
 
 @app.route('/')
@@ -22,42 +21,44 @@ def home():
 
 @app.route('/transactions/<date>')
 def get_transactions(date):
-    #go to db and find the 
-    print(date)
-    date_obj = datetime.strptime(date, '%d-%m-%y')
-    print(date_obj)
-    rd=date_obj.strftime('%d %b %y')
-    print(rd)
-    data = list(db.banktransactiondetails.find({ "Date": rd}))
-    for details in data:
-        details["_id"] = str(details["_id"])
-    return json.dumps(data)
+    try:
+        date_obj = datetime.strptime(date, '%d-%m-%y')
+        format_data = date_obj.strftime('%d %b %y')
+        data = list(db.banktransactiondetails.find({ "Date": format_data }))
+        if(data):
+            for value in data:
+                value["_id"] = str(value["_id"])
+            return json.dumps(data)
+        return f"No transactions on {date}"
+    except ValueError: 
+        return f"Check date format. {date} does not match format '%d-%m-%y'"
 
 @app.route('/balance/<date>')
 def get_balance(date):
-    print(date)
-    #go to db and find the 
-    date_obj = datetime.strptime(date, '%d-%m-%y')
-    print(date_obj)
-    rd=date_obj.strftime('%d %b %y')
-    print(rd)
-    data = list(db.banktransactiondetails.find({ "Date": rd}))
-    for i in data:
-        i["_id"] = str(i["_id"])
-    if(data):
-        balance=data[-1]["Balance AMT"]
-    else:
-        balance = "No transactions on that day"
-    print(balance)
-    return json.dumps(balance)
+    try:
+        date_obj = datetime.strptime(date, '%d-%m-%y')
+        format_data = date_obj.strftime('%d %b %y')
+        data = list(db.banktransactiondetails.find({ "Date": format_data}))
+        if(data):
+            for value in data:
+                value["_id"] = str(value["_id"])
+            balance = data[-1]["Balance AMT"]
+            return f"Balance at the end of {date} is: {balance}"
+        return f"No transactions on {date}"
+    except ValueError: 
+        return f"Check date format. {date} does not match format '%d-%m-%y'"
 
 @app.route('/details/<id>')
 def get_details(id):
-    objInstance = ObjectId(id)
-    data = db.banktransactiondetails.find_one({"_id" : objInstance})
-    print(data)
-    data["_id"] = str(data["_id"])
-    return json.dumps(data)
+    try:
+        objId = ObjectId(id)
+        data = db.banktransactiondetails.find_one({"_id" : objId})
+        if(data):
+            data["_id"] = str(data["_id"])
+            return json.dumps(data)
+        return f"No data Found for Id: {id}. Check Id"
+    except bson.errors.InvalidId:
+        return "Invalid Id. Id must be a valid BsonId"
 
 if __name__=="__main__":
     app.run(debug=True)
